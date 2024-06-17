2bb67e1365fc7bc32bcd20b88518c6db554cdbee
kqtCODE <85825734+kqtCODE@users.noreply.github.com>
2024-06-17 17:36:52 +0800

    Update urls.py

--- Project/authenticator/urls.py
+++ Project/authenticator/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.signup_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('api-token-auth', views.obtain_auth_token)
 ]
